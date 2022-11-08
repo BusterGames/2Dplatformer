7857a7ac23a17df8df7fbcb4c691c1d994095bad
BusterGames <cvetkovskin22@gmail.com>
2022-11-08 20:25:51 +0100

    enemy

--- scripts/entity/player.gd
+++ scripts/entity/player.gd
@@ -6,12 +6,12 @@ onready var target = position
 var velocity = Vector2()
 
 func _input(event):
-    if event.is_action_pressed("click"):
-        target = get_global_mouse_position()
+	if event.is_action_pressed("click"):
+		target = get_global_mouse_position()
 
 func _physics_process(_delta):
-    velocity = position.direction_to(target) * speed
-    # look_at(target)
-    if position.distance_to(target) > 5:
-        velocity = move_and_slide(velocity)
+	velocity = position.direction_to(target) * speed
+	# look_at(target)
+	if position.distance_to(target) > 5:
+		velocity = move_and_slide(velocity)
 	
